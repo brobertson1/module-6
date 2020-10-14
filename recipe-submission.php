cc572a8c213ea80b29c9adea18896524e7b03c99
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-10-14 17:19:45 -0400

    Set up recipe submission page.

--- recipe-submission.php
+++ recipe-submission.php
@@ -4,12 +4,22 @@
     include 'head.php';
 ?>
 <body>
-<div class="wrapper">
+
     <?php
         include 'header.php';
     ?>
-    <main>
 
+    <main>
+        <h2 class="recipe-submission-header">Add a Recipe to Our Cookbook!</h2>
+        <form method="POST" class="recipe-submission-form">
+            <input type="text" placeholder="Title" id="title" name="title">
+            <input type="text" placeholder="Author" id="author" name="author">
+            <!-- The textareas can be replaced with divs to use the Ace HTML editor -->
+            <textarea name="ingredients" id="ingredients" placeholder="Ingredients"></textarea>
+            <textarea name="directions" id="directions" placeholder="Directions"></textarea>
+
+            <input type="submit" class="button" id="submit" value="Submit">
+        </form>
     </main>
 
     <?php
@@ -17,6 +27,5 @@
         include 'scripts.php';
     ?>
 
-</div>
 </body>
 </html>
