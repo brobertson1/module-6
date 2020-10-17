2cd5421a05646e41d2c1d864df61925db86783af
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-10-17 11:50:16 -0400

    Updated db_connect and php for host database.

--- recipe.php
+++ recipe.php
@@ -14,35 +14,32 @@
     <?php
         function getRecipeDetailsFromDatabase() {
             $recipeTitle = rawurldecode($_GET["title"]);
-            
+
             include_once 'db_connect.php';
-            $sql = "SELECT * FROM posts WHERE title='" . $recipeTitle . "'";
+            $sql = "SELECT * FROM recipes WHERE title = '" . $recipeTitle . "'";
             $result = mysqli_query($conn, $sql);
 
             $recipeDetails = mysqli_fetch_assoc($result);
             return $recipeDetails;
         }
-    ?>
-<div>
-    <?php
         $recipeDetails = getRecipeDetailsFromDatabase();
     ?>
-
-        <h2 class="recipe-title"><?php echo $recipeDetails['title']; ?></h2>
+<div>
+        <h2 class="recipe-title"><?php echo $recipeDetails["title"]; ?></h2>
         <div class="recipe-author">
-            <p><?php echo $recipeDetails['author']; ?></p>
+            <p><?php echo $recipeDetails["author"]; ?></p>
         </div>
         <div class="recipe-content">
             <div class="ingredients-col">
                 <h3>Ingredients</h3>
                 <div class="recipe-ingredients">
-                    <p><?php echo $recipeDetails['ingredients']; ?></p>
+                    <p><?php echo $recipeDetails["ingredients"]; ?></p>
                 </div>
             </div>
             <div class="directions-col">
                 <h3>Directions</h3>
                 <div class="recipe-directions">
-                    <p><?php echo $recipeDetails ['directions']; ?></p>
+                    <p><?php echo $recipeDetails["directions"]; ?></p>
                 </div>
             </div>
         </div>
